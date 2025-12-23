019b831c98b0e30be6441d4c93f6a43c78129e3d
Aditya <adityas.singh.tech@gmail.com>
2025-12-23 14:23:41 +0530

    add bento grid and features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 
-import { Header,HeroSection } from "@/components/index";
+import { Header, HeroSection, Logos, BentoSection, SolutionSection, FeaturesSection, CollaborativeSection } from "@/components/index";
 export default function Home() {
   return (
     <>
-    <Header />
-    <HeroSection />
+      <Header />
+      <HeroSection />
+      <Logos />
+      <BentoSection />
+      <SolutionSection />
+      <FeaturesSection />
+      <CollaborativeSection />
     </>
   );
 }
